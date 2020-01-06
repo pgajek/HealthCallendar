c76a36a5fc7478ccc0ce61f06a72b1ca3d063659
redooo25 <redooo25@gmail.com>
2020-01-06 14:25:31 +0100

    paragraph

---
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { theme } from 'theme/mainTheme.js';
+
+const Paragraph = styled.p`
+  font-size: ${theme.fontSize.s};
+  font-weight: ${theme.regular};
+`;
+
+export default Paragraph;
---
+++ src/components/atoms/Paragraph/Paragraph.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Paragraph from './Paragraph';
+
+storiesOf('Atoms/Paragraph', module).add('Normal', () => <Paragraph>Hey callendar</Paragraph>);
