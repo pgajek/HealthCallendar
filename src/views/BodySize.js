a10e76fb610b09205f7f0fb90fbe871a791a7165
Patryk Gajek <pgajek94@gmail.com>
2020-04-10 18:16:10 +0200

    updated styles

--- src/views/BodySize.js
+++ src/views/BodySize.js
@@ -1,22 +1,81 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import FormTemplate from 'templates/FormTemplate.js';
+import IconButton from 'components/atoms/IconButton/IconButton';
 import Field from 'components/atoms/Field/Field.js';
+import MainTemplate from 'templates/MainTemplate.js';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { createDate } from 'helpers';
+import { ReactComponent as YesIcon } from 'assets/icons/yesicon.svg';
+import { ReactComponent as WrongIcon } from 'assets/icons/wrongicon.svg';
+import { ReactComponent as RunningGuy } from 'assets/Graphics/runningGuy.svg';
 
 const StyledWrapper = styled.div`
+  position: relative;
+  height: 100%;
+  min-height: 80vh;
+  width: 100%;
+
+  @media (orientation: landscape) {
+    min-height: 100vh;
+  }
+  @media (orientation: landscape) and (min-width: 1024px) {
+    min-height: 80vh;
+  }
+`;
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  padding-top: 4vh;
+  height: 100%;
+  width: 100%;
+  padding-bottom: 40vmin;
+
+  @media (orientation: portrait) and (min-width: 768px) {
+    padding-bottom: 30vmin;
+  }
+  @media (orientation: landscape) and (min-width: 1024px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 80%;
+    margin: 8vh auto 0 auto;
+  }
+  @media (orientation: landscape) and (min-width: 1280px) {
+    width: 90%;
+  }
+`;
+const StyledImage = styled(RunningGuy)`
   position: absolute;
-  top: 0;
+  bottom: -4vmax;
   left: 0;
+
+  height: 40vmin;
+  width: 40vmin;
+`;
+const StyledButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
   display: flex;
   align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 100vw;
-  min-height: 100vh;
-  height: 100%;
+  justify-content: space-around;
+
+  width: 40vmin;
+  height: 40vmin;
+  margin-right: 10px;
+  align-self: flex-end;
+
+  @media (orientation: portrait) and (min-width: 768px) {
+    margin-right: 10vmin;
+    width: 30vmin;
+  }
+  @media (orientation: landscape) and (min-width: 1024px) {
+    width: 30vmin;
+    margin-right: 10vmin;
+  }
 `;
 
 class BodySize extends Component {
@@ -33,7 +92,7 @@ class BodySize extends Component {
     haveSent: false,
   };
 
-  handleInputChange = e => {
+  handleInputChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
@@ -61,20 +120,20 @@ class BodySize extends Component {
         Authorization: `${token}`,
       },
     })
-      .then(res => {
-        this.setState(prevState => ({
+      .then((res) => {
+        this.setState((prevState) => ({
           ...prevState,
           haveSent: true,
         }));
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
   handleYesButtonClick = () => {
     this.handleDataPost();
   };
   componentDidMount() {
     const theDate = createDate();
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       ...prevState,
       date: theDate,
     }));
@@ -95,64 +154,79 @@ class BodySize extends Component {
       return <Redirect to="/home" />;
     }
     return (
-      <StyledWrapper>
-        <FormTemplate
-          header="Body Size"
-          yesClick={this.handleYesButtonClick}
-          wrongClick={this.hadnleLogIn}
-        >
-          <Field
-            name="armSize"
-            label="Arm Size"
-            unit="cm"
-            change={this.handleInputChange}
-            value={armSize}
-          />
-          <Field
-            name="bodyWeight"
-            label="Body Weight"
-            unit="kg"
-            change={this.handleInputChange}
-            value={bodyWeight}
-          />
-          <Field
-            name="bustSize"
-            label="Bust Size"
-            unit="cm"
-            change={this.handleInputChange}
-            value={bustSize}
-          />
-          <Field name="calf" label="Calf" unit="cm" change={this.handleInputChange} value={calf} />
-          <Field
-            name="femoralSize"
-            label="Femoral Size"
-            unit="cm"
-            change={this.handleInputChange}
-            value={femoralSize}
-          />
-          <Field
-            name="hipsSize"
-            label="Hips Size"
-            unit="cm"
-            change={this.handleInputChange}
-            value={hipsSize}
-          />
-          <Field
-            name="neckSize"
-            label="Neck Size"
-            unit="cm"
-            change={this.handleInputChange}
-            value={neckSize}
-          />
-          <Field
-            name="waist"
-            label="Waist"
-            unit="cm"
-            change={this.handleInputChange}
-            value={waist}
-          />
-        </FormTemplate>
-      </StyledWrapper>
+      <MainTemplate>
+        <StyledWrapper>
+          <StyledForm>
+            <Field
+              name="armSize"
+              label="Arm Size"
+              unit="cm"
+              change={this.handleInputChange}
+              value={armSize}
+            />
+            <Field
+              name="bodyWeight"
+              label="Body Weight"
+              unit="kg"
+              change={this.handleInputChange}
+              value={bodyWeight}
+            />
+            <Field
+              name="bustSize"
+              label="Bust Size"
+              unit="cm"
+              change={this.handleInputChange}
+              value={bustSize}
+            />
+            <Field
+              name="calf"
+              label="Calf"
+              unit="cm"
+              change={this.handleInputChange}
+              value={calf}
+            />
+            <Field
+              name="femoralSize"
+              label="Femoral Size"
+              unit="cm"
+              change={this.handleInputChange}
+              value={femoralSize}
+            />
+            <Field
+              name="hipsSize"
+              label="Hips Size"
+              unit="cm"
+              change={this.handleInputChange}
+              value={hipsSize}
+            />
+            <Field
+              name="neckSize"
+              label="Neck Size"
+              unit="cm"
+              change={this.handleInputChange}
+              value={neckSize}
+            />
+            <Field
+              name="waist"
+              label="Waist"
+              unit="cm"
+              change={this.handleInputChange}
+              value={waist}
+            />
+            <StyledButtonWrapper>
+              <IconButton onClick={this.handleYesButtonClick}>
+                <YesIcon />
+              </IconButton>
+              <IconButton>
+                <Link to="/home">
+                  <WrongIcon />
+                </Link>
+              </IconButton>
+            </StyledButtonWrapper>
+          </StyledForm>
+          <StyledImage />
+        </StyledWrapper>
+      </MainTemplate>
     );
   }
 }
@@ -160,5 +234,5 @@ const mapStateToProps = ({ userId, token }) => ({
   userId,
   token,
 });
-const mapDispatchToProps = dispatch => ({});
-export default connect(mapStateToProps, mapDispatchToProps)(BodySize);
+
+export default connect(mapStateToProps)(BodySize);
