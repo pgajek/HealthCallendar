d25c53af24ea54d9bc7a9b3c1ba084cce98e8f4b
Patryk Gajek <pgajek94@gmail.com>
2020-04-10 18:15:21 +0200

    new styles for homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,117 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Count from 'components/molecules/Count/Count';
 import MainTemplate from 'templates/MainTemplate.js';
 import { connect } from 'react-redux';
 import Button from 'components/atoms/Button/Button';
+import Controler from 'components/molecules/Controler/Controler';
 import { createDate } from 'helpers';
+import { ReactComponent as Logo } from 'assets/icons/logo.svg';
+import { ReactComponent as GirlRun } from 'assets/Graphics/girl_run.svg';
+import { theme } from 'theme/mainTheme.js';
+
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  height: 100%;
+
+  @media (orientation: landscape) {
+    min-height: 90vh;
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (orientation: landscape) and(min-width: 1280px) {
+    grid-template-columns: 70vw 30vw;
+  }
+`;
+
+const StyledGreenWrapper = styled.section`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+
+  height: 90vh;
+  padding-top: 25%;
+
+  background-color: ${theme.mainGreen};
+  & > * {
+    z-index: 10;
+    margin: 20px 0;
+  }
+  @media (orientation: landscape) {
+    width: 50vw;
+    height: 100%;
+  }
+  @media (min-width: 1024px) {
+    width: 50vw;
+    height: 100%;
+  }
+  @media (orientation: landscape) and (min-width: 1280px) {
+    width: 60vw;
+    height: 100%;
+    padding-bottom: 30px;
+  }
+`;
+const StyledHeader = styled.h1`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  height: 15%;
+  width: 100%;
+  margin: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #86d176;
+`;
+const StyledWhiteWrapper = styled.section`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  min-height: 90vh;
+  padding-bottom: 5vh;
+
+  & > * {
+    margin: 10px 0;
+  }
+
+  @media (orientation: landscape) {
+    width: 50vw;
+    height: 100%;
+  }
+  @media (min-width: 1024px) {
+    width: 50vw;
+    height: 100%;
+  }
+  @media (min-width: 1280px) {
+    width: 40vw;
+    height: 100%;
+  }
+`;
+const StyledGirl = styled(GirlRun)`
+  width: 20vmax;
+  height: 20vmax;
+
+  @media (min-width: 1024px) {
+    width: 15vmax;
+    height: 15vmax;
+    justify-self: flex-start;
+  }
+`;
 
 class Home extends Component {
   state = {
@@ -32,8 +140,10 @@ class Home extends Component {
       },
       body: JSON.stringify(day),
     })
-      .then(this.getDayData())
-      .catch(err => console.log(err));
+      .then((res) => {
+        this.getDayData();
+      })
+      .catch((err) => console.log(err));
   };
   getDayData = () => {
     const { userId } = this.props;
@@ -45,18 +155,20 @@ class Home extends Component {
         Authorization: `${token}`,
       },
     })
-      .then(res => {
+      .then((res) => {
         if (res.ok) {
           return res.json();
         } else {
           this.postDay();
         }
       })
-      .then(dayId => {
-        window.sessionStorage.setItem('dayId', dayId);
-        this.setState(prevState => ({ ...prevState, dayId }));
+      .then((dayId) => {
+        if (dayId) {
+          window.sessionStorage.setItem('dayId', dayId);
+          this.setState((prevState) => ({ ...prevState, dayId }));
+        }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
   downloadUserData = () => {
     const { userId } = this.props;
@@ -68,17 +180,19 @@ class Home extends Component {
         Authorization: `${token}`,
       },
     })
-      .then(res => res.json())
-      .then(data => {
-        console.log(this.state);
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const { lastDateMeasureBody, portionsSnack, portionsDrink, portionsAlcohol } = data;
         this.setState({
           ...this.state,
-          portionsAlcohol: data.portionsAlcohol,
-          portionsDrink: data.portionsDrink,
-          portionsSnack: data.portionsSnack,
+          portionsAlcohol: portionsAlcohol,
+          portionsDrink: portionsDrink,
+          portionsSnack: portionsSnack,
+          lastBodyMeasurement: lastDateMeasureBody,
         });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
   updateUserData = () => {
     const { userId, dayId } = this.props;
@@ -104,47 +218,71 @@ class Home extends Component {
   handleCount = (aspect, type) => {
     if (this.state[aspect] > -1 && this.state[aspect] < 8) {
       if (type === '+') {
-        console.log('counting');
-        this.setState(prevState => ({
+        this.setState((prevState) => ({
           ...prevState,
           [aspect]: this.state[aspect] + 1,
         }));
       } else if (this.state[aspect] > 0) {
-        console.log('counting');
-        this.setState(prevState => ({
+        this.setState((prevState) => ({
           ...prevState,
           [aspect]: this.state[aspect] - 1,
         }));
       }
     }
   };
-
+  checkMeasurementDate(apiString) {
+    const reg = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+    if (reg.test(apiString)) {
+      return apiString;
+    } else return 'None yet';
+  }
   render() {
-    const { portionsSnack, portionsDrink, portionsAlcohol } = this.state;
+    const { portionsSnack, portionsDrink, portionsAlcohol, lastBodyMeasurement } = this.state;
     return (
       <MainTemplate>
-        <Count
-          aspect="portionsDrink"
-          howMany={8}
-          count={portionsDrink}
-          click={this.handleCount}
-          sendData={this.updateUserData}
-        />
-        <Count
-          aspect="portionsAlcohol"
-          howMany={4}
-          count={portionsAlcohol}
-          click={this.handleCount}
-          sendData={this.updateUserData}
-        />
-        <Count
-          aspect="portionsSnack"
-          howMany={4}
-          count={portionsSnack}
-          click={this.handleCount}
-          sendData={this.updateUserData}
-        />
-        <Button onClick={this.updateUserData}>Send</Button>
+        <StyledWrapper>
+          <StyledGreenWrapper>
+            <StyledHeader>
+              <Logo />
+            </StyledHeader>
+            <Count
+              aspect="portionsDrink"
+              howMany={8}
+              count={portionsDrink}
+              click={this.handleCount}
+              sendData={this.updateUserData}
+            />
+            <Count
+              aspect="portionsAlcohol"
+              howMany={4}
+              count={portionsAlcohol}
+              click={this.handleCount}
+              sendData={this.updateUserData}
+            />
+            <Count
+              aspect="portionsSnack"
+              howMany={4}
+              count={portionsSnack}
+              click={this.handleCount}
+              sendData={this.updateUserData}
+            />
+            <Button onClick={this.updateUserData}>Update</Button>
+          </StyledGreenWrapper>
+          <StyledWhiteWrapper>
+            <StyledGirl />
+            <Controler label="Last body measurement">
+              <span>
+                {lastBodyMeasurement ? this.checkMeasurementDate(lastBodyMeasurement) : 'null'}
+              </span>
+            </Controler>
+            <Controler label="Last training">
+              <span>yyy-dd-mm</span> | <span>21:32</span>
+            </Controler>
+            <Controler label="Last meal">
+              <span>nazwaposilku</span> | <span>0000kcal</span> | <span>21.32</span>
+            </Controler>
+          </StyledWhiteWrapper>
+        </StyledWrapper>
       </MainTemplate>
     );
   }
