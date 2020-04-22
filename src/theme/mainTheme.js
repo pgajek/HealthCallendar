16e351b59c35eb91fafd4cfee8265ab331850c26
Patryk Gajek <pgajek94@gmail.com>
2020-04-22 09:20:42 +0200

    removed unnecessary values

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -1,14 +1,8 @@
 export const theme = {
   mainGreen: '#96E885',
-  // lightGreen: 'hsl(102.7, 64.6%, 61.2%)',
-  // shadowGreen: 'hsl(102.4, 74.5%, 63.1%)',
-  // darkTextShadowGreen: 'hsl(102.4, 78.1%, 64.1%)',
-  // lightTextShadowGreen: 'hsl(130.2, 71.3%, 35.5%)',
-  // darkGray: 'hsl(0, 0%, 29.4%)',
-  // lightGray: 'hsl(0, 0%, 86.3%)',
   grayBorder: 'rgb(155, 155, 155)',
   fontSize: {
-    xxs: '1.2 rem',
+    xxs: '1.2rem',
     xs: '1.4rem',
     s: '1.6rem',
     m: '2rem',
