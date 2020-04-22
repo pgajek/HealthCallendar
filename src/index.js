36fb7fad957eec6aa12523eb296e3e221884ff72
Patryk Gajek <pgajek94@gmail.com>
2020-04-22 13:10:01 +0200

    updating packages with private console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
+// import VConsole from 'vconsole';
+
+// var vConsole = new VConsole();
+// console.log('Hello world');
 
 ReactDOM.render(<Root />, document.getElementById('root'));
