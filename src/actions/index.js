d2772ca002c33ee3c80550d10dc1189d85c18521
Patryk Gajek <pgajek94@gmail.com>
2020-05-09 13:24:31 +0200

    new action declaration

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,13 @@
 export const authenticate = (user) => (dispatch) => {
-  return fetch('https://164.132.97.42:8443/health-calendar/login', {
+  return fetch('https://164.132.97.42:8443/health-calendar/token', {
     method: 'POST',
     body: JSON.stringify(user),
     headers: {
       'Content-type': 'application/json',
     },
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false,
   })
     .then((response) => {
       if (response.ok) {
@@ -21,4 +24,49 @@ export const authenticate = (user) => (dispatch) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+export const getDayId = (userId, apiToken, date) => (dispatch) => {
+  const token = JSON.parse(JSON.stringify(`Bearer ${apiToken}`));
+  fetch(`https://164.132.97.42:8443/health-calendar/api/day/day-id/${date}/${userId}`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `${token}`,
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((dayId) => {
+      if (dayId) {
+        window.localStorage.setItem('dayId', dayId);
+        dispatch({ type: 'GET_DAY_ID', payload: dayId });
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+export const createNewDay = (userId, apiToken, date) => (dispatch) => {
+  const day = {
+    date,
+    portionsAlcohol: 0,
+    portionsDrink: 0,
+    portionsSnack: 0,
+    userId,
+  };
+  const token = JSON.parse(JSON.stringify(`Bearer ${apiToken}`));
+  return fetch(`https://164.132.97.42:8443/health-calendar/api/day`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `${token}`,
+    },
+    body: JSON.stringify(day),
+  })
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: 'GET_DAY_ID', payload: data }))
+    .catch((err) => console.log(err));
 };
